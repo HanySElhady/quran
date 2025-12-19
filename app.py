81a4d2b58d7deb48d5b21fc610a59a046ec1b268
HanySElhady <46346727+HanySElhady@users.noreply.github.com>
2025-12-19 15:33:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ elif search_type == "عرض السورة كاملة":
 
     if selected_surah == "القرآن كله":
         df_display = df.sort_values(
-            ["surah_name","ayah_number"]
+            ["surah_name"]
         ).reset_index(drop=True)
 
     for _, row in df_display.iterrows():
@@ -222,3 +222,4 @@ elif search_type == "عرض السورة كاملة":
             """,
             unsafe_allow_html=True
         )
+
